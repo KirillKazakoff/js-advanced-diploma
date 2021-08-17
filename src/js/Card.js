6864eac2945acbb19aae8b59495a58e6f1daf439
keka3avr <kekazavr228@gmail.com>
2021-08-17 14:33:37 +1100

    console

--- src/js/Card.js
+++ src/js/Card.js
@@ -5,6 +5,7 @@ export default class Card {
         this.mana = this.content.querySelector('.mana');
         this.health = this.content.querySelector('.health');
         this.attack = this.content.querySelector('.attack');
+        console.log('hello');
     }
                     
 
