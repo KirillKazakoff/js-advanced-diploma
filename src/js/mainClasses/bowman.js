603934aa8924fe2fc15e7ac1155f267ce7707ee0
keka3avr <kekazavr228@gmail.com>
2021-08-17 20:00:01 +1100

    bowman props fixed

--- src/js/mainClasses/bowman.js
+++ src/js/mainClasses/bowman.js
@@ -3,7 +3,7 @@ import Character from '../classes/main';
 export default class Bowman extends Character {
     constructor(level) {
         super(level, 'bowman');
-        this.attack = 50;
+        this.attack = 25;
         this.defence = 25;
         this.moveRange = 2;
         this.attackRange = 2;
