eeab5f7c903d1a6d4ac62019c581f4ce19b9665a
keka3avr <kekazavr228@gmail.com>
2021-08-17 15:27:18 +1100

    onMenuClick lost

--- src/js/Card.js
+++ src/js/Card.js
@@ -1,11 +1,26 @@
+
 export default class Card {
     constructor(turn) {
         this.content = document.querySelector(`.${turn}-card`);
-        this.char = this.content.querySelector('.mana');
+        this.char = this.content.querySelector('.character');
         this.mana = this.content.querySelector('.mana');
         this.health = this.content.querySelector('.health');
         this.attack = this.content.querySelector('.attack');
     }
                     
+    showCharacter(char) {
+        let { type, health, mana, attack } = char;
+        
+        this.char.className = `character ${type} char-in-card`;
+        this.mana.textContent = mana;
+        this.health.textContent = health;
+        this.attack.textContent = attack;
+    }
 
+    showGeneric() {
+        this.char.className = 'character generic char-in-card';
+        this.mana.textContent = '';
+        this.health.textContent = '';
+        this.attack.textContent = '';
+    }
 }
