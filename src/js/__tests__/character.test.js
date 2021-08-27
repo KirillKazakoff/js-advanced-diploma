1cd9325e4bb4cb4b48ad4d293de1e399b15ff04c
keka3avr <kekazavr228@gmail.com>
2021-08-27 13:44:22 +1100

    test fixed

--- src/js/__tests__/character.test.js
+++ src/js/__tests__/character.test.js
@@ -1,22 +1,18 @@
-jest.mock('../gamePlay');
-jest.mock('../gameState');
-
 import Character from '../classes/main';
-
-
-
-beforeEach(() => {
-    jest.resetAllMocks();
-});
 
 jest.mock('../gamePlay', () => ({
     methodToMock: 0,
 }));
-import { methodToMock } from 'the-package-to-mock'
+jest.mock('../gameState', () => ({
+    methodToMock2: 0,
+}));
+import { methodToMock } from '../gamePlay'
+import { methodToMock2 } from '../gameState';
 
 
 test('can\'t invoke parent class', () => {
-    methodToMock.mockImplementation(() => 0);
+    // methodToMock.mockImplementation(() => 0);
+    // methodToMock2.mockImplementation(() => 0);
     const check = () => new Character();
     expect(check).toThrow();
 });
